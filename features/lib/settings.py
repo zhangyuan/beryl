7baf223083c4adab0f9ed39413590fe05d1993b4
Yuan Zhang <zhangyuan@users.noreply.github.com>
2015-11-29 20:24:11 +0800

    simplify code by using or

--- features/lib/settings.py
+++ features/lib/settings.py
@@ -1,11 +1,7 @@
 import os
 
-app_env = os.environ.get('APP_ENV')
-
-if app_env == None:
-  app_env = "dev"
-else:
-  app_env = app_env.strip()
+app_env = os.environ.get('APP_ENV') or "dev"
+app_env = app_env.strip()
 
 if app_env == "dev":
   products_url = "http://localhost:8000/"
