67402c6c21ba80892cfd4a3511a42382c7eaf61e
Yuan Zhang <zhangyuan@users.noreply.github.com>
2015-11-29 19:24:09 +0800

    handle different environments

--- features/lib/settings.py
+++ features/lib/settings.py
@@ -1,1 +1,15 @@
-products_url = "http://localhost:8000/"
+import os
+
+app_env = os.environ['APP_ENV']
+
+if app_env == None:
+  app_env = "dev"
+else:
+  app_env = app_env.strip()
+
+if app_env == "dev":
+  products_url = "http://localhost:8000/"
+elif app_env == "prod":
+  products_url = "http://zhangyuan.github.io/databases/"
+else:
+  raise Exception("Invalid APP_ENV:", app_env)
